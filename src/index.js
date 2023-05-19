d2b3a1e13dd9674573e39f746487f4a7773b2a9b
galobponce <personal.galo.bello@gmail.com>
2023-05-19 17:29:19 -0300

    Handle event listeners and Add JSDoc

--- src/index.js
+++ src/index.js
@@ -13,18 +13,83 @@ export const gvdom = (type, props, ...args) => {
   }
 }
 
+/**
+ * Checks if a property is an event.
+ * @param {string} prop
+ * @return {boolean} true if it is an event, false otherwise
+ */
+const isEventProp = (prop) => {
+  return /^on/.test(prop)
+}
+
+/**
+ * Special treatment for boolean props.
+ *
+ * This is because in cases like checked={false} in JSX
+ * are transformed into checked="false" wich is actually true.
+ * @param {HTMLElement} $element
+ * @param {string} prop
+ * @param {boolean} value
+ */
+const setBooleanProp = ($element, prop, value) => {
+  if (value) {
+    $element[prop] = true
+  } else {
+    $element[prop] = false
+  }
+}
+
+/**
+ * Parses an event name.
+ *
+ * Example: "onClick" => "click"
+ * @param {string} eventName
+ * @return {string}
+ */
+const extractEventName = (eventName) => {
+  return eventName.slice(2).toLowerCase()
+}
+
+/**
+ * Add the attributes from the props to the given element.
+ * @param {HTMLElement} $element
+ * @param {object} props
+ */
 const setAttributes = ($element, props) => {
   // If there is no props
   if (!Object.keys(props).length) return
 
-  Object.keys(props).map((prop) => {
-    $element.setAttribute(prop, props[prop])
-  })
+  Object.keys(props)
+    .filter((prop) => !isEventProp(prop))
+    .map((prop) => {
+      $element.setAttribute(prop, props[prop])
+
+      if (typeof props[prop] === "boolean") {
+        setBooleanProp($element, prop, props[prop])
+      }
+    })
 }
 
 /**
- * Creates a DOM node.
+ * Add the event listeners from the props to the given element.
+ * @param {HTMLElement} $element
+ * @param {object} props
+ */
+const addEventListeners = ($element, props = {}) => {
+  if (!props) {
+    return
+  }
+  Object.keys(props)
+    .filter(isEventProp)
+    .forEach((event) =>
+      $element.addEventListener(extractEventName(event), props[event])
+    )
+}
+
+/**
+ * Creates a DOM element given a JSX node.
  * @param {*} node
+ * @return {HTMLElement} the node converted into a DOM element
  */
 export const createElement = (node) => {
   // Handler for functional components
@@ -38,6 +103,8 @@ export const createElement = (node) => {
   const $element = document.createElement(node.type)
 
   setAttributes($element, node.props)
+
+  addEventListeners($element, node.props)
 
   // Uses recursion to create and append its children to the element
   node.children &&
