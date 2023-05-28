77af57809453dbf4c2e1d62438648ebaa2df9dfc
galobponce <personal.galo.bello@gmail.com>
2023-05-28 15:54:03 -0300

    Add element updates with its props and events

--- src/index.js
+++ src/index.js
@@ -54,6 +54,17 @@ const addBooleanProp = ($element, prop, value) => {
 }
 
 /**
+ * Removes a boolean prop.
+ *
+ * @param {HTMLElement} $element
+ * @param {string} prop the prop's name
+ */
+const removeBooleanProp = ($element, prop) => {
+  $element.removeAttribute(prop)
+  $element[prop] = false
+}
+
+/**
  * Parses an event name.
  *
  * Example: "onClick" => "click"
@@ -96,19 +107,41 @@ const addProp = ($element, prop, value) => {
 }
 
 /**
+ * Removes a given prop from an element.
+ *
+ * @param {HTMLElement} $element
+ * @param {string} prop the prop's name
+ * @param {*} value the prop's value
+ */
+const removeProp = ($element, prop, value) => {
+  if (isCustomProp(prop)) {
+    return
+  } else if (typeof value === "boolean") {
+    removeBooleanProp($element, prop)
+  } else {
+    $element.removeAttribute(prop)
+  }
+}
+
+/**
  * Add the event listeners from the props to the given element.
  * @param {HTMLElement} $element
  * @param {object} props
  */
 const addEventListeners = ($element, props) => {
-  // If there is no props
-  if (!Object.keys(props).length) return
-
   Object.keys(props)
     .filter(isEventProp)
-    .map((event) =>
-      $element.addEventListener(extractEventName(event), props[event])
-    )
+    .map((event) => addEventListener($element, event, props[event]))
+}
+
+/**
+ * Add the given event listener and its value to the given element.
+ * @param {HTMLElement} $element
+ * @param {string} event
+ * @param {EventListenerOrEventListenerObject} value
+ */
+const addEventListener = ($element, event, value) => {
+  $element.addEventListener(extractEventName(event), value)
 }
 
 /**
@@ -138,4 +171,130 @@ export const createElement = (node) => {
     })
 
   return $element
+}
+
+/**
+ * Checks if a two nodes are different or not.
+ * @param {*} node
+ * @return {boolean} true if they are, false otherwise
+ */
+const areNodesDifferent = (node1, node2) => {
+  return (
+    typeof node1 !== typeof node2 ||
+    (typeof node1 === "string" && node1 !== node2) ||
+    node1.type !== node2.type
+  )
+}
+
+/**
+ * Updates an HTML element's prop depending on difference between the values of the prop passed
+ * @param {HTMLElement} $element the parent of the nodes
+ * @param {*} prop
+ * @param {*} newValue
+ * @param {*} oldValue
+ */
+const updateProp = ($element, prop, newValue, oldValue) => {
+  if (!newValue) {
+    removeProp($element, prop, oldValue)
+  } else if (!oldValue || newValue !== oldValue) {
+    addProp($element, prop, newValue)
+  }
+}
+
+/**
+ * Updates an HTML element's props depending on difference between the props passed
+ * @param {HTMLElement} $element the parent of the nodes
+ * @param {object} newProps
+ * @param {object} oldProps
+ */
+const updateProps = ($element, newProps, oldProps) => {
+  const props = Object.assign({}, newProps, oldProps)
+  Object.keys(props).map((name) => {
+    updateProp($element, name, newProps[name], oldProps[name])
+  })
+}
+
+/**
+ * Updates the event listeners from the props to the given element re-adding them.
+ * @param {HTMLElement} $element
+ * @param {object} newProps
+ * @param {object} oldProps
+ */
+const updateEventListeners = ($element, newProps, oldProps) => {
+  // Gets the event listeners from the props
+  const props = Object.assign({}, newProps, oldProps)
+  const events = new Set(Object.keys(props).filter((prop) => isEventProp(prop)))
+
+  events.forEach((event) => {
+    updateEventListener(
+      $element,
+      extractEventName(event),
+      newProps[event],
+      oldProps[event]
+    )
+  })
+}
+
+/**
+ * Updates the event listeners from the props to the given element re-adding them.
+ * @param {HTMLElement} $element
+ * @param {string} event the name of the event
+ * @param {EventListenerOrEventListenerObject} newValue
+ * @param {EventListenerOrEventListenerObject} oldValue
+ */
+const updateEventListener = ($element, event, newValue, oldValue) => {
+  if (!newValue) {
+    $element.removeEventListener(event, oldValue)
+  } else if (!oldValue) {
+    $element.addEventListener(event, newValue)
+  } else {
+    $element.removeEventListener(event, oldValue)
+    $element.addEventListener(event, newValue)
+  }
+}
+
+/**
+ * Updates an HTML element depending on difference between the nodes passed
+ * @param {HTMLElement} $element the parent of the nodes
+ * @param {*} newNode
+ * @param {*} oldNode
+ * @param {number} index the node's index from the parent's childNodes list
+ */
+export const updateElement = ($element, newNode, oldNode, index = 0) => {
+  // If the old node doesn't exist, it adds the new one to the parent.
+  if (!oldNode) {
+    $element.appendChild(createElement(newNode))
+  }
+  // If the new node doesn't exist, it removes the child at index from the parent.
+  else if (!newNode) {
+    $element.removeChild($element.childNodes[index])
+  }
+  // If the nodes have changed, it replaces the old one with its new version.
+  else if (areNodesDifferent(newNode, oldNode)) {
+    $element.replaceChild(createElement(newNode), $element.childNodes[index])
+  }
+  // If the node is not a text node
+  else if (newNode.type) {
+    // Updates its props
+    updateProps($element.childNodes[index], newNode.props, oldNode.props)
+    // Updates its events
+    updateEventListeners(
+      $element.childNodes[index],
+      newNode.props,
+      oldNode.props
+    )
+
+    const newLength = (newNode.children && newNode.children.length) || 0
+    const oldLength = (oldNode.children && oldNode.children.length) || 0
+
+    // Recursively updates its children
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      updateElement(
+        $element.childNodes[index],
+        newNode.children[i],
+        oldNode.children[i],
+        i
+      )
+    }
+  }
 }
